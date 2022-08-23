3ba3997df90575b294d8bca02171eb7b63f07abe
fhlottideveloper <fhlottideveloper@gmail.com>
2022-08-23 05:46:17 -0300

    Create Entity Global Entity for use inheritance

--- src/Domain/Products/Products.cs
+++ src/Domain/Products/Products.cs
@@ -1,13 +1,9 @@
 ﻿namespace IWantApp.Domain.Products;
 
-public class Products
+public class Products : Entity
 {
-    public Guid Id { get; set; }
     public string Name { get; set; }
     public Category Description { get; set; }
     public bool HasStock { get; set; }
-    public string CreateBy { get; set; }
-    public DateTime CreateOn { get; set; }
-    public string EditedBy { get; set; }
-    public DateTime EditedOn { get; set; }
+    
 }
