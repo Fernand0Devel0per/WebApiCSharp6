d00e27053543f4f6fec830bba1907b208392be8f
fhlottideveloper <fhlottideveloper@gmail.com>
2022-08-28 15:57:08 -0300

    pagination with dapper

--- src/Endpoints/Employees/EmployeeGetAll.cs
+++ src/Endpoints/Employees/EmployeeGetAll.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Dapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Data.SqlClient;
 using System.Security.Claims;
 
 namespace IWantApp.Endpoints.Employee;
@@ -11,7 +13,23 @@ public class EmployeeGetAll
 
     public static Delegate Handle => Action;
 
-    public static IResult Action(int page, int rows, UserManager<IdentityUser> userManager)
+    public static IResult Action(int? page, int? rows, IConfiguration configuration)
+    {
+        var db = new SqlConnection(configuration["ConnectionString:IWantDb"]);
+        var employees = db.Query<EmployeeResponse>(
+            @"select Email, ClaimValue as Name
+                from AspNetUsers u inner
+                join AspNetUserClaims c
+                on u.id = c.UserId and claimtype = 'Name'
+                Order by name
+                OFFSET (@page -1) * @rows Rows Fetch Next @rows Rows Only", new {page, rows }
+            );
+        return Results.Ok(employees);
+    
+    }
+
+    /* Consulta sem performance
+     public static IResult Action(int page, int rows, UserManager<IdentityUser> userManager)
     {
         var users = userManager.Users.Skip((page-1)*rows).Take(rows).ToList();
         var employees = new List<EmployeeResponse>();
@@ -25,5 +43,6 @@ public class EmployeeGetAll
 
         return Results.Ok(employees);
     }
+     */
 
 }
